1676e50bfa64807139180539f59712df517f36da
apple <apple@appledeMacBook-Air.local>
2015-07-31 10:24:12 +0800

    Initial Commit

---
+++ QRCodeScan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCodeScan
+//
+//  Created by apple on 15/7/31.
+//  Copyright (c) 2015年 com.agewnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
